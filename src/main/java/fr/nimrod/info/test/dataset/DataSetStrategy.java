bf38d058402a4f06d7a6727e8c3a4867b707ccf1
primael <primael@l-infini.Fr>
2014-09-12 12:03:01 +0200

    Fix
    Propagation des exceptions.

--- src/main/java/fr/nimrod/info/test/dataset/DataSetStrategy.java
+++ src/main/java/fr/nimrod/info/test/dataset/DataSetStrategy.java
@@ -1,16 +1,14 @@
 package fr.nimrod.info.test.dataset;
 
-import lombok.SneakyThrows;
-
 import org.dbunit.dataset.AbstractDataSet;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 
 import fr.nimrod.info.test.dataset.implementation.JsonDataSet;
 
 public interface DataSetStrategy {
 
-	@SneakyThrows
-	static AbstractDataSet getImplementation(String file, Class<?> resourceBase) {
+	static AbstractDataSet getImplementation(String file, Class<?> resourceBase) throws DataSetException {
 		if(file.endsWith(".json")){
 			return new JsonDataSet(resourceBase.getResourceAsStream(file));
 		}
